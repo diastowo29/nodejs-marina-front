04636b39e405f4d267d602e94f8f39876afd7ad5
Diastowo <diastowo@gmail.com>
2025-01-15 17:51:04 +0700

    test flight

--- src/app/api/chat/[chat]/comments/route.ts
+++ src/app/api/chat/[chat]/comments/route.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient()
 
 export async function GET(request:Request, { params } : any) {
-    const comments = await prisma.omnichat_line.findMany({
-        where: {
-            omnichatId: Number.parseInt(params.chat)
-        }
-    });
-    return Response.json(comments);
+    // const comments = await prisma.omnichat_line.findMany({
+    //     where: {
+    //         omnichatId: Number.parseInt(params.chat)
+    //     }
+    // });
+    // return Response.json(comments);
+    return Response.json({});
 }
