a53117baee8855c421d32d86121561390a76a869
Diastowo <diastowo@gmail.com>
2025-06-11 11:37:09 +0700

    test lb

--- src/app/actions/sign/actions.ts
+++ src/app/actions/sign/actions.ts
@@ -3,6 +3,5 @@ import { createHmac } from "crypto";
 
 export const generateHmac = async (message:string, secret:string) => {
     let signString = await createHmac('sha256', secret).update(message).digest('hex');
-    console.log(signString)
     return signString;
 }
