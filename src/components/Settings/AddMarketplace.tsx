5e194f8eaeb8c50dcaeca421b3dfc64f964bfca9
diastowo <diastowo@gmail.com>
2026-04-16 14:09:18 +0700

    fixing redirect

--- src/components/Settings/AddMarketplace.tsx
+++ src/components/Settings/AddMarketplace.tsx
@@ -49,9 +49,7 @@ export default function AddMarketplace(props:any) {
                 if (tiktokWindow?.closed) {
                     clearInterval(timer);
                 }
-                console.log(process.env.APP_BASE_URL);
-                console.log(tiktokWindow?.location);
-                if (tiktokWindow?.location.href.startsWith(process.env.APP_BASE_URL || 'http://localhost:3000')) {
+                if (tiktokWindow?.location.href.startsWith('https://marina-apps-553781175495.asia-southeast2.run.app')) {
                     const windowUrl = new URL(tiktokWindow.location.href);
                     let channel = '';
                     let authResponse = {};
@@ -74,6 +72,8 @@ export default function AddMarketplace(props:any) {
                     }
                     tiktokWindow.close();
                     clearInterval(timer);
+                } else {
+                    console.log(tiktokWindow?.location);
                 }
             } catch (e) {
                 if (e instanceof DOMException && e.name === "SecurityError") {
